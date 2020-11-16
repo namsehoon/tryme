689266a7741dd3db40821e0b0a90dc4b9661154d
nhoon95 <skatpgnsx@naver.com>
2020-11-16 10:53:16 +0900

    complete

--- app/src/main/java/TryMe/gohool/tryme/MainActivity.java
+++ app/src/main/java/TryMe/gohool/tryme/MainActivity.java
@@ -1,14 +1,45 @@
 package TryMe.gohool.tryme;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    private View window_view;
+    private Button Btn;
+    private int[] colors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        colors = new int[]{Color.RED,Color.BLUE, Color.BLACK, Color.YELLOW};
+
+        window_view = (View) findViewById(R.id.windowViewId);
+        Btn = (Button) findViewById(R.id.btn);
+
+
+
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random random = new Random();
+                int ran_num = random.nextInt(colors.length);
+
+                window_view.setBackgroundColor(colors[ran_num]);
+                Log.d("random", String.valueOf(ran_num));
+
+            }
+        });
+
     }
 }
